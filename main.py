0edb70cec9d2bdc016a6b293e6815a91b56f4312
kertser <kertser@yahoo.com>
2022-01-05 11:07:54 +0200

    Minor fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/",responce_class = HTMLResponse)
+@app.get("/", response_class = HTMLResponse)
 async def main():
     content = """
     <marquee width="525" behavior="alternate"><h1 style="color:red;font-family:Arial">Please set your Water UVT254</h1></marquee>
