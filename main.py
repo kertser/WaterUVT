37bbc06fcca7cfb511d7f44b7f94432072f87bfe
kertser <kertser@yahoo.com>
2022-01-05 11:03:49 +0200

    Added fastAPI

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/",responce_class = HTMLResponse)
+async def main():
+    content = """
+    <marquee width="525" behavior="alternate"><h1 style="color:red;font-family:Arial">Please set your Water UVT254</h1></marquee>
+    """
+    return content
