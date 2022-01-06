c0e7416247f4ccea5c0cc875983ca96190b1401d
kertser <kertser@yahoo.com>
2022-01-06 14:42:32 +0200

    Some fixes

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="htmldirectory")
 filename = "UVT_output.csv"
-
+defaultUVT = 65
 
 @app.get("/", response_class=HTMLResponse)
-async def main(request: Request, waterUVT=65):
+async def main(request: Request, waterUVT=defaultUVT):
     WaterCalculator.getWaterVector(waterUVT)
     # print(Save_to_CSV)
     return templates.TemplateResponse("home.html", {"request": request, "waterUVT": waterUVT})
 
 
 @app.post("/")
-def handle_UVT(request: Request, UVT: int = Form(...), Save_to_CSV: bool = Form(False)):
-    WaterCalculator.getWaterVector(UVT,True)
-    if Save_to_CSV is True:
-        FileResponse(path=filename, filename=filename, media_type='text/csv')
-    return templates.TemplateResponse('home.html',  {"request": request, "waterUVT": UVT, "Save_to_CSV": Save_to_CSV})
+async def handle_UVT(request: Request, UVT: int = Form(defaultUVT), Save_to_CSV: str = Form('')):
+    WaterCalculator.getWaterVector(UVT, True)
+    if Save_to_CSV == 'Save to CSV file':
+        return FileResponse(path=filename, filename=filename, media_type='text/csv')
+    else:
+        return templates.TemplateResponse('home.html',  {"request": request, "waterUVT": UVT, "Save_to_CSV": Save_to_CSV})
