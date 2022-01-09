07df45e8197c5592a5eaf141d1ca8db38680dedd
kertser <kertser@yahoo.com>
2022-01-09 16:35:31 +0200

    Release 1.2c Added lignin-sulfonate concentration calculation

--- main.py
+++ main.py
@@ -14,14 +14,20 @@ defaultUVT = 65
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request, waterUVT=defaultUVT):
     WaterCalculator.getWaterVector(waterUVT)
+    A_Lignine = -1.3559
+    B_Lignine = 125.57
+    Lignin = round(A_Lignine*waterUVT+B_Lignine)
     # print(Save_to_CSV)
-    return templates.TemplateResponse("home.html", {"request": request, "waterUVT": waterUVT})
+    return templates.TemplateResponse("home.html", {"request": request, "waterUVT": waterUVT, "Lignin": Lignin})
 
 
 @app.post("/")
 async def handle_UVT(request: Request, UVT: int = Form(defaultUVT), Save_to_CSV: str = Form('')):
     WaterCalculator.getWaterVector(UVT, True)
+    A_Lignine = -1.3559
+    B_Lignine = 125.57
+    Lignin = round(A_Lignine*UVT+B_Lignine)
     if Save_to_CSV == 'Save to CSV file':
         return FileResponse(path=filename, filename=filename, media_type='text/csv')
     else:
-        return templates.TemplateResponse('home.html',  {"request": request, "waterUVT": UVT, "Save_to_CSV": Save_to_CSV})
+        return templates.TemplateResponse('home.html',  {"request": request, "waterUVT": UVT, "Lignin": Lignin, "Save_to_CSV": Save_to_CSV})
