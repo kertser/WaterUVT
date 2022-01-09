56b7179fb884ac9c3705f043ac5295f768bbab50
kertser <kertser@yahoo.com>
2022-01-09 16:09:41 +0200

    Release 1.2b Fixed indices

--- WaterCalculator.py
+++ WaterCalculator.py
@@ -11,11 +11,13 @@ from numpy import linspace as Linspace
 from pandas import DataFrame as DataFrame
 import matplotlib.pyplot as plt
 
-def getWaterVector(TargetUVT=90, OutputToCSV=False): # Calculate the TargetUVT is in[%-1cm]
-    WavelengthRange = Linspace(0.190, 0.600, 411)
+# import plotly.express as px
+
+def getWaterVector(TargetUVT=90, OutputToCSV=False):  # Calculate the TargetUVT is in[%-1cm]
+    WavelengthRange = Linspace(0.180, 0.600, 421)
 
     # Calculate the Prediction
-    TargetVector = [[wl, TargetUVT/100] for wl in WavelengthRange]
+    TargetVector = [[wl, TargetUVT / 100] for wl in WavelengthRange]
     PredictionT = model.predict(scaler.transform(DataFrame(TargetVector)))
 
     # Make the plot
@@ -25,24 +27,35 @@ def getWaterVector(TargetUVT=90, OutputToCSV=False): # Calculate the TargetUVT i
     ax1.set_xlabel('wavelength [nm]')
     ax1.set_ylabel('UVT[%-1cm]')
     ax1.grid(color='g', linestyle='--', linewidth=0.5)
-    ax1.scatter(WavelengthRange, PredictionT,s=1)
+    ax1.scatter(WavelengthRange*1000, PredictionT*100, s=1)
 
-    ax1.annotate('UVT254 = '+str(TargetUVT)+'[%-1cm]', xy=(1, 0), xycoords='axes fraction', style='italic', fontsize=12,
-                horizontalalignment='right', verticalalignment='bottom',bbox={'facecolor': 'green', 'alpha': 0.5, 'pad': 1})
+    ax1.annotate('UVT254 = ' + str(TargetUVT) + '[%-1cm]', xy=(1, 0), xycoords='axes fraction', style='italic',
+                 fontsize=12,
+                 horizontalalignment='right', verticalalignment='bottom',
+                 bbox={'facecolor': 'green', 'alpha': 0.5, 'pad': 1})
     fig.savefig('static/UVTvsWavelength.png', bbox_inches='tight')
 
+    """ Some interactive charts
+    
+    fig = px.scatter(df, x="wavelength [nm]", y="UVT [%-1cm]")
+    fig = px.scatter(x=WavelengthRange, y=[item for sublist in PredictionT for item in sublist])
+    fig.update_xaxes(title_text='wavelength[nm]')
+    fig.update_yaxes(title_text='UVT[%-1cm]')
+    fig.write_html("aaa.html")
+    """
     # Output the model into CSV file
-    if (OutputToCSV==True):
+    if OutputToCSV is True:
         Output = DataFrame()
-        Output['Wavelength'] = DataFrame(WavelengthRange)
-        Output['UVT'] = DataFrame(PredictionT)
+        Output['Wavelength'] = DataFrame(WavelengthRange*1000)
+        Output['UVT'] = DataFrame(PredictionT*100)
         filename = 'UVT_output.csv'
         Output.to_csv(filename, index=False)
 
-#%% Main script
+
+# %% Main script
 # Load the model and the Scaler
 
-#model = keras.models.load_model('WaterUVT.h5')
+# model = keras.models.load_model('WaterUVT.h5')
 model = load_model('WaterUVT.h5')
 scaler = load(open('scaler.pkl', 'rb'))
 
