02a0f1824c6c471825f416ff6265f21a18efbf57
kertser <kertser@yahoo.com>
2022-01-10 08:54:54 +0200

    Release 1.2d Updated lignin-sulfonate concentration calculation

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import WaterCalculator
 
+A_Lignin = 0.0072
+B_Lignin = -2.1
+C_Lignin = 138
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="htmldirectory")
 filename = "UVT_output.csv"
 defaultUVT = 65
 
+
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request, waterUVT=defaultUVT):
     WaterCalculator.getWaterVector(waterUVT)
-    A_Lignine = -1.3559
-    B_Lignine = 125.57
-    Lignin = round(A_Lignine*waterUVT+B_Lignine)
+
+    Lignin = round(A_Lignin*waterUVT**2+B_Lignin*waterUVT+C_Lignin)
     # print(Save_to_CSV)
     return templates.TemplateResponse("home.html", {"request": request, "waterUVT": waterUVT, "Lignin": Lignin})
 
@@ -24,9 +28,7 @@ async def main(request: Request, waterUVT=defaultUVT):
 @app.post("/")
 async def handle_UVT(request: Request, UVT: int = Form(defaultUVT), Save_to_CSV: str = Form('')):
     WaterCalculator.getWaterVector(UVT, True)
-    A_Lignine = -1.3559
-    B_Lignine = 125.57
-    Lignin = round(A_Lignine*UVT+B_Lignine)
+    Lignin = round(A_Lignin*UVT**2+B_Lignin*UVT+C_Lignin)
     if Save_to_CSV == 'Save to CSV file':
         return FileResponse(path=filename, filename=filename, media_type='text/csv')
     else:
